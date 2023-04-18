6958d99531083868cab18ee9f2653b2fc121e613
Saksham Madan <sakshammadan@protonmail.com>
2023-04-18 11:21:13 +0530

    Accept CLA

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+#![allow(non_snake_case)]
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Utilising String, not OsString, cause of simplicity and portability.
+    // OsString values differ per platform and are more complex to work with than String values.
+    let args: Vec<String> = env::args().collect();
+
+    //args[0] is binary name, which dont need for the proejct. We will ignore it.
+
+    let query = &args[1]; //String needed to be searched
+    let file_path = &args[2]; //Input Text File
+
+    println!("Searchin for {query}");
+    println!("In text file {file_path}");
+    
 }
