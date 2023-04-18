bfff71bf696e3247ac31f63d732b81d076019985
Saksham Madan <sakshammadan@protonmail.com>
2023-04-18 11:38:32 +0530

    Reading from File

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
 use std::env;
+use std::fs;
 
 fn main() {
     // Utilising String, not OsString, cause of simplicity and portability.
@@ -12,7 +13,13 @@ fn main() {
     let query = &args[1]; //String needed to be searched
     let file_path = &args[2]; //Input Text File
 
-    println!("Searchin for {query}");
+    println!("Searchin for '{query}'");
     println!("In text file {file_path}");
+
+    // Reading a file
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
     
 }
