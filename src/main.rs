2512a0e0cf10660c001836b55dcec68bad094223
Saksham Madan <sakshammadan@protonmail.com>
2023-04-19 12:52:33 +0530

    Some error handling

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,37 @@
 
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     file_path: String
 }
 
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
 
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-fn parse_config(args: &[String]) -> Config {
+        //args[0] is binary name, which dont need for the proejct. We will ignore it.
+        let query = args[1].clone(); //String needed to be searched
+        let file_path = args[2].clone(); //Input Text File
 
-    //args[0] is binary name, which dont need for the proejct. We will ignore it.
-    let query = args[1].clone(); //String needed to be searched
-    let file_path = args[2].clone(); //Input Text File
-
-    Config { query, file_path }
+        Ok(Config { query, file_path})
+    }
 }
+
 
 fn main() {
     // Utilising String, not OsString, cause of simplicity and portability.
     // OsString values differ per platform and are more complex to work with than String values.
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem Parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searchin for '{}'", config.query);
     println!("In text file {}", config.file_path);
